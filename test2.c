ac5844d3547e4aee6df65d08919398fc22a45be1
HankDaly <18868637597@163.com>
2018-08-10 02:32:21 +0800

    20180810

--- test2.c
+++ test2.c
@@ -1,10 +1,21 @@
 // copyright [2018] <mituh>
-// 分数化小数(decimal) - 数组可进位版本
-#include <stdio.h>
-#define MAXN 100 + 5
+// 分数化小数(decimal) - 数组可进位版
+#include<iostream>
+#include<algorithm>
+#include<string>
+#include<sstream>
+#include<vector>
+#include<set>
+#include<map>
+
+using namespace std;
+#define MAXN 100 
 
 
 int main(){
-    printf("%d",'F'-'A');
+    map<vector<int>,int> aaa;
+    vector<int> qqq = {1,2,3,4};
+    aaa[qqq] = 1;
+    cout << aaa[qqq] << endl;
     return 0;
 }
