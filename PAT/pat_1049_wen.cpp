2123d7f1432f66a88f0a0bd06e19122e8d7ceb0c
HankDaly <18868637597@163.com>
2018-08-24 00:34:22 +0800

    20180824

--- PAT/pat_1049_wen.cpp
+++ PAT/pat_1049_wen.cpp
@@ -5,18 +5,21 @@
 #include<cctype>
 #include<cmath>
 #include<algorithm>
-#include<vector>
 
 using namespace std;
-vector<int> ve;
+double ch[100005];
+
 main(){
 #ifdef LOCAL
     freopen("input.in","r",stdin);
     freopen("output.out","w",stdout);
-#endif
-
-    ve.push_back(1);
-    cout << *ve.begin();
-       
+#endif 
+    int N; cin >> N;
+    double sum = 0.0;
+    for(int i = 1;i<=N;i++){
+        cin >> ch[i];
+        sum += (N-i+1)*ch[i]*i;
+    }
+    printf("%.2f\n",sum);
     return 0;
 }
--- test2.cpp
+++ test2.cpp
@@ -4,25 +4,41 @@
 #include<cctype>
 #include<algorithm>
 #include<string>
+#include<cmath>
+#include<vector>
 using namespace std;
 
+long ch[100005];
+vector<long> ve;
 int main(){
 #ifdef LOCAL 
     freopen("input.in","r",stdin);
     freopen("output.out","w",stdout);
-#endif 
-    string a = "string";
-    int b = a.size();
-    for(int i = 0;i<b;i++){
-        for(int q = i+1;q<b;q++){
-            string temp_1;
-            cout << temp_1 + a[i] + a[q] << endl;
+#endif
+    memset(ch,0,sizeof(ch));
+    int N; cin >> N;
+    for(int i = 0;i<N;i++){
+        int temp;
+        scanf("%d",&temp);
+        ch[i] = temp;
+    }
+    for(int i = 0;i<N;i++){
+        int ok = 1;
+        for(int z = i+1;z<N;z++){
+            if(ch[i] > ch[z]){ok = 0;}
         }
-        for(int z = i-1;z>=0;z--){
-            string temp_2;
-            cout << temp_2+a[i]+a[z] << endl;
+        for(int k = i-1;k>=0;k--){
+            if(ch[i] < ch[k]){ok = 0;}
         }
+        if(ok == 1){ve.push_back(ch[i]);}
+    }
+    sort(ve.begin(),ve.end());
+    int kase = 0;
+    cout << ve.size() << "\n";
+    for(auto a = ve.begin();a!=ve.end();a++){
+        if(kase != 0){cout << " ";}
+        cout << *a;
+        kase = 1;
     }
     return 0;
-    
 }
