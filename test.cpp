5ec930c1e9889b3e5c5a31e95c273a88185dccb4
HankDaly <18868637597@163.com>
2018-08-27 01:55:02 +0800

    20180827

--- test.cpp
+++ test.cpp
@@ -7,28 +7,25 @@
 #include<vector>
 #include<cmath>
 using namespace std;
-long ch[100005];
-vector<long> ve;
-main(){
+int ch_p[100005];
+int main(){
 #ifdef LOCAL
     freopen("input.in","r",stdin);
     freopen("output.out","w",stdout);
 #endif
-    int N; cin >> N;
-    long p; cin >> p;
-    int max = 0;
-    for(int i = 0;i<N;i++){
-        long temp; cin >> temp;
-        ch[temp] = 1;
-        ve.push_back(temp);
+    string A; cin >> A;
+    int A_len = A.size();
+    int sum = 0;
+    int p_n = 0;
+    for(int i = 0;i<A_len;i++){
+        if(A[i] == 'P'){p_n++;ch_p[i] = p_n;}
+        else{ch_p[i] = p_n;}
     }
-    for(int i = 0;i<N;i++){
-        int cnt = 0;
-        for(int z = ve[i];z<ve[i]*p;z++){
-            if(ch[z] == 1){cnt++;}
-        }
-        if(cnt >= max){max = cnt;}
+    int t_n = 0;
+    for(int i = A_len-1;i>=0;i--){
+        if(A[i] == 'T'){t_n++;}
+        if(A[i] == 'A'){sum = (sum+t_n*ch_p[i])%1000000007;}
     }
-    cout << max;
+    cout << sum;
     return 0;
 }
