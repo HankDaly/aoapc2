b70131c2a1829dd61b1e7800874063ce6c7fd15f
HankDaly <18868637597@163.com>
2018-09-05 23:20:41 +0800

    delete pat

--- test.cpp
+++ test.cpp
@@ -7,58 +7,15 @@
 #include<vector>
 #include<cmath>
 using namespace std;
-int ch_value[100005];
-int ch_last[100005];
+
 int main(){
 #ifdef LOCAL
     freopen("input.in","r",stdin);
     freopen("output.out","w",stdout);
-#endif
-    int first_node,K,N; cin >> first_node >> N >> K;
-    for(int i = 0;i<N;i++){
-        int temp; cin >> temp;
-        cin >> ch_value[temp] >> ch_last[temp];
-    }
-    //首先先来一遍小于0循环
-    int temp_node = first_node;
-    int kase = 0;
-    while(temp_node != -1){
-        if(ch_value[temp_node] < 0 && kase == 0){
-            printf("%05d %d",temp_node,ch_value[temp_node]);
-            ++kase;
-        }
-        else if(ch_value[temp_node] < 0 && kase > 0){
-            printf(" %05d\n%05d %d",temp_node,temp_node,ch_value[temp_node]);
-        }
-        else{}
-        temp_node = ch_last[temp_node];
-    }
-    //第二遍就是0-K之间
-    temp_node = first_node; // temp_node重置
-    while(temp_node != -1){
-        if(ch_value[temp_node] >= 0 && ch_value[temp_node] <= K && kase == 0){
-            printf("%05d %d",temp_node,ch_value[temp_node]);
-            ++kase;
-        }
-        else if(ch_value[temp_node] >= 0 && ch_value[temp_node] <= K && kase == 1){
-            printf(" %05d\n%05d %d",temp_node,temp_node,ch_value[temp_node]);
-        }
-        else{}
-        temp_node = ch_last[temp_node];
-    }
-    //第三遍大于K
-    temp_node = first_node; // temp_node重置
-    while(temp_node != -1){
-        if(ch_value[temp_node] > K && kase == 0){
-            printf("%05d %d",temp_node,ch_value[temp_node]);
-            ++kase;
-        }
-        else if(ch_value[temp_node] > K && kase == 1){
-            printf(" %05d\n%05d %d",temp_node,temp_node,ch_value[temp_node]);
-        }
-        else{}
-        temp_node = ch_last[temp_node];
-    }
-    printf(" -1");
+#endif 
+    string a = "2";
+    int b = 2;
+    a += to_string(b);
+    cout << a;
     return 0;
 }
--- test1.cpp
+++ test1.cpp
@@ -1,71 +1,24 @@
 #include<iostream>
-#include<string>
 #include<algorithm>
+#include<string>
 #include<cstring>
-#include<cstdio>
 using namespace std;
-
-struct Node{
-    int No;
-    int score;
-    int all_n;
-    int true_n;
-    int t_selected = 0;
-    int wrong = 0;
-};
-bool cmp(Node a,Node b){
-    if(a.wrong == b.wrong){return a.No<b.No;}
-    else{return a.wrong>b.wrong;}
-}
-Node ch[105];
-int ha[] = {1,2,4,8,16};
-int main()
-{
+int main(){
 #ifdef LOCAL
     freopen("input.in","r",stdin);
     freopen("output.out","w",stdout);
 #endif
-    memset(ch,0,sizeof(ch));
-    int N,M; cin >> N >> M;
-    for(int i = 0;i<M;i++){
-        Node node; cin >> node.score >> node.all_n >> node.true_n;
-        node.No = i+1;
-        for(int z = 0;z<node.true_n;z++){
-            string temp; cin >> temp;
-            node.t_selected += ha[(temp[0]-'a')];
-        }
-        ch[i] = node;
-    }
-    //题目整理完毕
+    int N; cin >> N;
+    int flr = 0,sum = 0;
     for(int i = 0;i<N;i++){
-        char a;
-        int kase = 0,student_sc = 0;
-        while((a = getchar()) != EOF){
-            int selected = 0;
-            if(a == '('){
-                int number; cin >> number;
-                for(int z = 0;z<number*2;z++){
-                    char c; c = getchar();
-                    if(c != ' '){selected += ha[c-'a'];}
-                }
-                if(selected^ch[kase].t_selected){ //如果不是零，代表有不一样的
-                    ++ch[kase].wrong;
-                }
-                else{student_sc += ch[kase].score;}
-                ++kase;
-            }
-            if(kase == M){cout << student_sc << "\n";break;}
+        int next_floor; cin >> next_floor;
+        int move = next_floor - flr;
+        if(move<0){
+            sum += move*(-1)*4+5;
         }
+        else{sum += move*6 + 5;}
+        flr = next_floor;
     }
-    // 学生统计完
-    sort(ch,ch+105,cmp);
-    if(ch[0].wrong == 0){cout << "Too simple";}
-    else{
-        cout << ch[0].wrong;
-        for(int i = 0;ch[i].wrong == ch[0].wrong;i++){
-            cout <<" "<< ch[i].No;
-        }
-    }
-    
-	return 0;
+    cout << sum;
+    return 0;
 }
