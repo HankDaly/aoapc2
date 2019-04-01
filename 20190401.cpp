5b93e1f787ffb66c797b5e113fb5b4f47068f2f6
HankDaly <18868637597@163.com>
2019-04-01 13:41:52 +0800

    20190401

---
+++ 20190401.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<algorithm>
+#include<cstring>
+#include<string>
+#include<cctype>
+#include<queue>
+
+using namespace std;
+
+
+BiNode* Search_SmallestK(BiNode* bt,int k){
+    int right;
+    if(bt->right == NULL){left = 0;}
+    else{right = bt->right->count;}
+    int now = bt->count - right;
+    //判断是否是K
+    if(now == k){return bt;}
+    //如果当前大于k
+    else if(now > k){bt->left->count = now-2;return Search_SmallestK(bt->left,k);}
+    else{bt->right->count = bt->count+1;return Search_SmallestK(bt->right,k);}
+}
+
+int main(){
+#ifdef LOCAL 
+    freopen("input.in","r",stdin);
+    freopen("output.out","w",stdout);
+#endif
+}
